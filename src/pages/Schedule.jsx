776f90051f523e621c2ae8ff0c65894e973bec44
JesusNoodl <maxshallam@gmail.com>
2025-11-10 17:49:16 +0000

    Changed Scedule page to use api

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { getAllClasses } from '../services/api';
+import { getAllClasses } from '../utils/api';
 
 function Schedule() {
   const [classes, setClasses] = useState([]);
