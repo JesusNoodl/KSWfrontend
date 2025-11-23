9782232bd63cbd10209715e97efc722ccb0940b5
JesusNoodl <maxshallam@gmail.com>
2025-11-23 10:11:51 +0000

    Added contacts page

--- src/pages/member/MemberContacts.jsx
+++ src/pages/member/MemberContacts.jsx
@@ -1,6 +1,6 @@
 // src/pages/MemberContacts.jsx
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../../contexts/AuthContext';
 import { 
   getMyContacts, 
   createContact, 
